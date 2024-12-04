f41a51d923e3e774f179aa0d2cae1102eddef87d
jared <jaredfanji2019@163.com>
2024-12-04 14:02:34 +0800

    优化Center类：引入JdbcFruit，改用ArrayList存储水果

--- view/Center.java
+++ view/Center.java
@@ -2,12 +2,14 @@ package FruitShop.view;
 
 import FruitShop.Entity.Fruit;
 import FruitShop.FruitShop;
+import FruitShop.dao.JdbcFruit;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import static FruitShop.FruitShop.localUrl;
@@ -22,8 +24,8 @@ public class Center {
     public int width, height;
     //格子面板
     public JPanel GridJPanel;
-    //数组
-    public JPanel[] JPfruits;
+    //面板数组
+    public  JPanel[] JPfruits;
     //当前页码
     public int PageNum = 1;
     //总页码
@@ -50,9 +52,9 @@ public class Center {
     }
 
     //把水果包装为面板
-    public JPanel[] PackFruits(Fruit[] fruits, boolean state) {
+    public JPanel[] PackFruits(ArrayList<Fruit> fruits, boolean state) {
         int i = 0;
-        JPanel[] jPanels = new JPanel[fruits.length];
+        JPanel[] jPanels = new JPanel[fruits.size()];
         for (Fruit fruit : fruits) {
             JPanel jPanel = new JPanel();
             //自定义布局
@@ -85,12 +87,11 @@ public class Center {
             jPanel.add(jLabel);
 
             // 为每个水果添加点击事件
-            int finalI = i;
             jPanel.addMouseListener(new MouseListener() {
                 @Override
                 public void mouseClicked(MouseEvent e) {
                     // 创建新的窗口，展示水果的详细信息
-                    fruitDetail.loadView(fruits[finalI]);
+                    fruitDetail.loadView(fruit);
                 }
                 @Override
                 public void mousePressed(MouseEvent e) {
@@ -153,29 +154,6 @@ public class Center {
         GridJPanel.repaint();
     }
 
-    /*//【删除】指定id的水果面板
-    public void removeID(int id) {
-        //删除控件数组
-        this.JPfruits = jpArrDel(id);
-        f5();
-        //更新页面总数(向上取整)
-        this.PageSum = JPfruits.length / (rows * cols) + (JPfruits.length % (rows * cols) != 0 ? 1 : 0);
-    }
-
-    //【删除】数组中某一项
-    private JPanel[] jpArrDel(int id) {
-        JPanel[] J = JPfruits;
-        JPanel[] JJ = new JPanel[J.length - 1];
-        for (int i = 0; i < J.length - 1; i++) {
-            if (i == id - 1) {
-                JJ[i] = J[i + 1];
-            } else {
-                JJ[i] = J[i];
-            }
-        }
-        return JJ;
-    }*/
-
     //【修改】数组中某一项
     private JPanel[] jpArrXg(JPanel x, int id) {
         JPanel[] J = JPfruits;
@@ -188,55 +166,5 @@ public class Center {
             }
         }
         return JJ;
-    }
-
-    //判断并修改相应属性
-    public void XgJPShu(Fruit[] fruits, int id, String name, String imgUrl, double pri, double sto) {
-        this.JPfruits = jpArrXg(PackFruits(new Fruit[]{new Fruit(id, name.equals("#") ? fruits[id - 1].getName() : name, imgUrl.equals("#") ? fruits[id - 1].getImgUrl() : imgUrl, pri < 0 ? fruits[id - 1].getPrice() : pri, sto < 0 ? fruits[id - 1].getStock() : sto)}, false)[0], id);
-        f5();
-    }
-
-    //水果数组压栈
-    private Fruit[] FPush(Fruit[] fruits, Fruit fruit) {
-        int s = fruits.length + 1;
-        Fruit[] fruits1 = Arrays.copyOf(fruits, s);
-        fruits1[s - 1] = fruit;
-        return fruits1;
-    }
-
-    //水果面板数组压栈
-    private JPanel[] jPush(JPanel[] jPanels, JPanel jPanel) {
-        int s = jPanels.length + 1;
-        JPanel[] jPane = Arrays.copyOf(jPanels, s);
-        jPane[s - 1] = jPanel;
-        return jPane;
-    }
-
-    //添加
-    public void TjFruit(Fruit fruit) {
-        //更新水果数组（保证修改时的水果新鲜）
-        FruitShop.fruits = FPush(FruitShop.fruits,fruit);
-        //把Fruit[0]打包为JPanel[0],false表示不更新JPfruits,只拿来转换
-        this.JPfruits = jPush(JPfruits, PackFruits(new Fruit[]{fruit}, false)[0]);
-        f5();
-    }
-    //删除
-    public void DelFruit(int id){
-        //判断删除的id后面还有无水果，有(则为后面的重新编号，重新打包，重新刷新),无(则直接删除，重新打包，重新刷新)
-        Fruit[] newFruits = Arrays.copyOf(FruitShop.fruits, FruitShop.fruits.length - 1);
-        for (int i = 0; i < FruitShop.fruits.length - 1; i++) {
-            if (i + 1 >= id){
-                //重设设置其id
-                FruitShop.fruits[i + 1].setId(i + 1);
-                newFruits[i] = FruitShop.fruits[i + 1];
-            }else{
-                newFruits[i] = FruitShop.fruits[i];
-            }
-        }
-        //更新水果数组（保证修改时的水果新鲜）
-        FruitShop.fruits = newFruits;
-        //打包,false表示不更新JPfruits等,只拿来转换
-        this.JPfruits = PackFruits(newFruits, false);
-        f5();
     }
 }
