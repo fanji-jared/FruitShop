0979bcf086b02bd6abb7e5bd7e5666bbf012ddc2
jared <jaredfanji2019@163.com>
2024-12-05 10:33:42 +0800

    添加客户ID列点击事件及优化FruitDetail界面

--- util/viewFruitDetailListen.java
+++ util/viewFruitDetailListen.java
@@ -4,6 +4,11 @@ import FruitShop.Entity.Fruit;
 import FruitShop.view.FruitDetail;
 
 import javax.swing.*;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.DefaultTableModel;
+
+import static FruitShop.dao.JdbcFruit.getCustomerData;
 
 public class viewFruitDetailListen {
     // 分页 - 当前页数
@@ -59,6 +64,28 @@ public class viewFruitDetailListen {
                 FruitDetail.refScrollPane(nowFruit);
             }else {
                 JOptionPane.showMessageDialog(null, "已经是最后一页了", "提示", JOptionPane.WARNING_MESSAGE);
+            }
+        });
+    }
+
+    /**
+     * 为表格 “客户ID” 添加点击事件
+     * @param model 表格控件
+     */
+    public static void AddListeners(DefaultTableModel model){
+        // 表格控件
+        model.addTableModelListener(new TableModelListener() {
+            @Override
+            public void tableChanged(TableModelEvent e) {
+                if (e.getColumn() == 2) {
+                    // 获取当前行的客户ID
+                    int customerId = (int) model.getValueAt(e.getFirstRow(), 2);
+                    // 获取当前行的客户信息
+                    Object[][] objects = getCustomerData(customerId);
+                    // 拼接客户详情
+                    String customerDetail = "客户ID：" + objects[0][0] + "\n客户姓名：" + objects[0][1] + "\n客户电话：" + objects[0][2];
+                    JOptionPane.showMessageDialog(null, customerDetail, "客户详情", JOptionPane.INFORMATION_MESSAGE);
+                }
             }
         });
     }
